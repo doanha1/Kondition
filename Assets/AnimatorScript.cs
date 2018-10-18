524244a701435f8e3825db7194f5014e61095486
Doan Ha <trong.ha@elev.ga.lbs.se>
2018-10-18 10:50:13 +0200

    animation

---
+++ Assets/AnimatorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (anim.GetBool("bool") == false)
+            {
+                anim.SetBool("bool", true);
+            }
+            else
+            {
+                anim.SetBool("bool", false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            anim.SetTrigger("trigger");
+        }
+    }
+}
